48b02e03d4d73c33f5874697375ace5d697c04b5
LaimonasAR <laimonas.paura@icloud.com>
2023-05-29 20:48:42 +0300

    cleaned comments

--- querrying.py
+++ querrying.py
@@ -10,33 +10,13 @@ from pymongo import MongoClient
 from pymongo.collection import Collection
 from datetime import timedelta, date
 
-# from db_generator import DbGenerator
-
-# connection = DbGenerator(host="0.0.0.0", port = 27017, db_name="Grocerie_Store", coll= "Groceries")
-# db = connection.connect_to_mongodb()
-
 client = MongoClient("mongodb://0.0.0.0:27017/")
 db = client["Store"]
 collection = db["Items"]
 
-# collection = DbGenerator(host="0.0.0.0", port = 27017, db_name="Store", coll= "Items")
-
-
 def filter_documents(
     collection: Collection, field_name: str, eq_value: str
 ) -> List[dict]:
-    """
-    Filters documents in a MongoDB collection based on field values using $eq and $ne operators.
-
-    Args:
-        collection: The MongoDB collection to filter.
-        field_name: The name of the field to filter.
-        eq_value: The equal value to match.
-        ne_value: The not equal value to exclude.
-
-    Returns:
-        A list of documents that match the filter criteria.
-    """
     query = {field_name: {"$eq": eq_value}}
     result = collection.find(query)
     return list(result)
@@ -57,21 +37,6 @@ def filter_documents_by_price(
     result = collection.find(query)
     return list(result)
 
-
-# print(
-#     filter_documents(
-#         collection=collection, field_name="Category", eq_value="Food", ne_value="Fish"
-#     )
-# )
-
-
-# def filter_electronics(collection: Collection, field_name: str, eq_value: str, ne_value: str) -> List[dict]:
-#         query = {
-#         field_name: {
-#             "$eq": eq_value,
-#             "$ne": ne_value
-#         }
-#     }
 def get_electronics():
     my_filter = filter_documents(
         collection=collection, field_name="Category", eq_value="Electronics"
@@ -133,7 +98,6 @@ def get_items_in_price_range():
     my_filter = filter_documents_by_price_range(
         collection=collection, field_name="Price", gt_value=10, lt_value=100
     )
-    # print(my_filter)
     for item in my_filter:
         if item["Name"].startswith("c"):
             print(item)
